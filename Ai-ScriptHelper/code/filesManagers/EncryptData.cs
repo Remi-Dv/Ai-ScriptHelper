ff03ff2f5e33a0025094b718a0ff255f6ff6dbf8
Remi-Dv <remi.gentils@gmail.com>
2023-09-07 21:48:46 +0200

    added encryption and decrypt

--- Ai-ScriptHelper/code/filesManagers/EncryptData.cs
+++ Ai-ScriptHelper/code/filesManagers/EncryptData.cs
@@ -1,10 +1,78 @@
-﻿using System;
-using System.Security.Cryptography;
+﻿using System.Security.Cryptography;
 using System.Text;
-using System.ComponentModel;
-using System.Management;
 
 public class EncryptData
 {
-    
+    public byte[] EncryptObject(string _objectName,byte[] _dataToEncrypt)
+    {
+        string selectedPassword;
+
+        Console.WriteLine($"Choose a password (remember it) to encrypt the file {_objectName}");
+        Console.Write("Password: ");
+
+        selectedPassword = Console.ReadLine();
+
+        byte[] selectedKey = ConvertStringToKey(selectedPassword);
+
+        using (Aes aesAlg = Aes.Create())
+        {
+            aesAlg.Key = selectedKey;
+            aesAlg.Mode = CipherMode.CFB; // CFB encryption
+
+            using (MemoryStream msEncrypt = new MemoryStream())
+            {
+                using (ICryptoTransform encryptor = aesAlg.CreateEncryptor())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        csEncrypt.Write(_dataToEncrypt, 0, _dataToEncrypt.Length);
+                        csEncrypt.FlushFinalBlock();
+                        return msEncrypt.ToArray();
+                    }
+                }
+            }
+        }
+    }
+
+    public byte[] DecryptObject(string _objectName, byte[] _dataToDecrypt)
+    {
+        string selectedPassword;
+
+        Console.WriteLine($"Write the password to decrypt the file {_objectName}");
+        Console.Write("Password: ");
+
+        selectedPassword = Console.ReadLine();
+
+        byte[] selectedKey = ConvertStringToKey(selectedPassword);
+
+        using (Aes aesAlg = Aes.Create())
+        {
+            aesAlg.Key = selectedKey;
+            aesAlg.Mode = CipherMode.CFB; // Assurez-vous d'utiliser le même mode de chiffrement qu'à l'encryption.
+
+            using (MemoryStream msDecrypt = new MemoryStream(_dataToDecrypt))
+            {
+                using (ICryptoTransform decryptor = aesAlg.CreateDecryptor())
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (MemoryStream msPlain = new MemoryStream())
+                        {
+                            csDecrypt.CopyTo(msPlain);
+                            return msPlain.ToArray();
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private byte[] ConvertStringToKey(string _stringToConvert)
+    {
+        using (SHA256 sha256 = SHA256.Create())
+        {
+            byte[] inputBytes = Encoding.UTF8.GetBytes(_stringToConvert);
+            return sha256.ComputeHash(inputBytes);
+        }
+    }
 }
