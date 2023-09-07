fb5d0028e393fab912b5cfa898694f8915d9fe1e
Remi-Dv <remi.gentils@gmail.com>
2023-09-07 18:07:58 +0200

    added saving directory

---
+++ Ai-ScriptHelper/code/filesManagers/FilesStorage.cs
@@ -0,0 +1,20 @@
+﻿
+
+public class FilesStorage
+{
+    private string projectName;
+    private string directoryPath;
+
+    public FilesStorage(string _projectName)
+    {
+        projectName = _projectName;
+        directoryPath = Path.Combine(Environment.GetFolderPath
+            (Environment.SpecialFolder.ApplicationData), "RemiDv", projectName);
+        CreateDirectories();
+    }
+
+    private void CreateDirectories()
+    {
+        Directory.CreateDirectory(directoryPath);
+    }
+}
